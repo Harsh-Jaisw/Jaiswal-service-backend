fb2088b574d80a77a976670c419fbb073498d3a8
Bhavish Muneshwar <Bhavishm009@gmail.com>
2024-06-15 16:11:09 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const account = require("./account");
+const common = require("./common");
+
+
 
 account(router);
+common(router);
 
 module.exports = router;
 
