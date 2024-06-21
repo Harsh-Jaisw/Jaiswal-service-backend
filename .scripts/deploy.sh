6b73f1753c7d1d704c213d2350235ccab379d26c
Bhavish <114305915+Bhavishm009@users.noreply.github.com>
2024-06-21 19:37:09 +0530

    new deploy

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,21 +1,39 @@
+# #!/bin/bash
+# set -e
+
+# echo "Deployment started ..."
+
+# # Pull the latest version of the app
+# git pull origin deploy
+# echo "New changes copied to server !"
+
+
+# echo "Check Node Js."
+# npm -v
+
+# echo "Installing Dependencies..."
+# npm install --yes
+
+
+# echo "Building the app..."
+# pm2 reload js-backend
+
+# echo "Deployment Finished........!"
+
+
 #!/bin/bash
-set -e
 
-echo "Deployment started ..."
+echo "Navigating to directory..."
+cd /home/youruser/htdocs/www.testing.harshadkajale.online || exit
 
-# Pull the latest version of the app
+echo "Cleaning the directory..."
+git reset --hard
+git clean -fd
+
+echo "Pulling the latest changes..."
 git pull origin deploy
-echo "New changes copied to server !"
 
-
-echo "Check Node Js."
-npm -v
-
-echo "Installing Dependencies..."
-npm install --yes
-
-
-echo "Building the app..."
+echo "Restarting the web server..."
 pm2 reload js-backend
 
-echo "Deployment Finished........!"
+echo "Deployment complete!"
