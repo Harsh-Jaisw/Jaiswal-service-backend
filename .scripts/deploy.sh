90e69a92c87d3326bf31296abf47ed34e2fb002d
Bhavish <114305915+Bhavishm009@users.noreply.github.com>
2024-06-21 23:32:58 +0530

    Update deploy.sh

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,34 +1,13 @@
-# #!/bin/bash
-# set -e
-
-# echo "Deployment started ..."
-
-# # Pull the latest version of the app
-# git pull origin main
-# echo "New changes copied to server !"
-
-# echo "Installing Dependencies..."
-# npm install --yes
-
-# echo "Deployment Finished!"
-
-
-
 #!/bin/bash
-
-# Check if npm is installed
-if ! command -v npm &> /dev/null
-then
-    echo "npm could not be found, installing Node.js and npm..."
-    # Install Node.js and npm
-    curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-    sudo apt-get install -y nodejs
-fi
+set -e
 
 echo "Deployment started ..."
-git pull origin main
-echo "New changes copied to server!"
-echo "Installing Dependencies..."
-npm install
 
-echo "Deployment completed!"
+# Pull the latest version of the app
+git pull origin main
+echo "New changes copied to server !"
+
+echo "Installing Dependencies..."
+npm install --yes
+
+echo "Deployment Finished!"
